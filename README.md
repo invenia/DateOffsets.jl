6bd44e049686f2a14ee69edcc49b42d6e9042897
Gem Newman <spurll@gmail.com>
2016-03-17 16:58:55 -0500

    Refactored. Added examples and rationale to README.
    Lots of structural changes are still happening.
    This is still very much a work in progress, but I've tried to document
    my thought process as much as possible. —Gem

--- README.md
+++ README.md
@@ -8,19 +8,91 @@
 [![codecov.io](https://codecov.io/github/invenia/Horizons.jl/coverage.svg?branch=master)](https://codecov.io/github/invenia/Horizons.jl?branch=master)
 ```
 
-`Horizons.jl` provides generators for forecast input and output offsets. Given a base `DateTime` (or any other `TimeType`) (or any other `TimeType`) (or any other `TimeType`) (or any other `TimeType`) (or any other `TimeType`) (or any other `TimeType`) (or any other `TimeType`) (or any other `TimeType`) (or any other `TimeType`) and some rules a `Task` (like a Python generator) is created that produces `DateTimes`s accordingly.
+`Horizons.jl` provides generators for forecast input and output offsets. Given a base
+`DateTime` (or any other `TimeType`) and some rules, a `Task` (like a Python generator) is
+created that produces `DateTimes`s accordingly.
+
+Here's a link to this feature's [specification document](https://gitlab.invenia.ca/invenia/replatforming/blob/master/feature-specs/horizons.md).
+
+**TODO:** The tasks/generators for horizons could be replaced by simple `StepRange`s, now
+that Gem got those working for `ZonedDateTime`s, but `StepRange`s are too simplistic to work
+for the dynamic data offsets (offsets that can be defined like, "most recent data available
+for the same hour of week as the target"). The plan is currently to use tasks for everything,
+but this is still kind of up in the air right now. (Another possibility would be to separate
+horizons and static/dynamic data offsets again, but as Gem and Curt discussed it seems like
+a good idea to couple these two concepts.)
 
 ## Overview
 
-The way horizons are dealt with in the Matlab Autopredictor are problematic. If we want to generate forecasts for each hour of a given day in a region that observes DST, the forecast will need to be configured for 25 horizons, and then only 24 (or 23) of these horizons are used.
+The way horizons are dealt with in the Matlab Autopredictor are problematic. If we want to
+generate forecasts for each hour of a given day in a region that observes DST, the forecast
+will need to be configured for 25 horizons, and then only 24 (or 23) of these horizons are
+used.
 
-Horizon `Task`s are designed to address these issues, replacing the following components of the Matlab Autopredictor:
+In the old Matlab code we would see something like this:
 
-  * Forecast **horizons**. For a given due (batch) date, the Matlab Autopredictor would make predictions for a series of horizons, each of which were `timedelta`s (`Period`s). These horizons would eventually be added to the due (batch) dates to obtain the target date. Instead of returning `Period`s, `Horizons.jl` returns `DateTime`s (or `ZonedDateTime`s) directly.
-  * Static and dynamic **offsets**. When defining the data source features to use as model input, the Matlab Autopredictor took a base date (either the due date or the target date of the forecast) and for both static (e.g., three hours) and dynamic (e.g., same hour of the week) offsets.
+```matlab
+due_date = date('1 January 2016 03:00', 'America/Winnipeg')
+horizons = timedelta(22:45, 'hours')    % Meant to be each hour of the next day.
+target_dates = due_date + horizons      % 2 January 2016 01:00 through 3 January 2016 00:00.
+```
 
-**TODO:** Ensure that we can have offets based on target date and offsets based on due date (and they're not likely to be confused with each other).
+This code has several problems, many involving DST transitions, where the value of the
+horizons (and even the number of horizons!) will vary. For example:
+
+```matlab
+due_date = date('12 March 2016 03:00', 'America/Winnipeg')
+horizons = timedelta(22:45, 'hours')    % Meant to be each hour of the next day.
+target_dates = due_date + horizons      % 13 March 2016 01:00 through 14 March 2016 01:00.
+```
+
+We end up with an extra hour that we don't want or need due to the DST transition. To
+correct the DST-related problems, we might do something like this in Julia:
+
+```julia
+using TimeZones
+due_date = ZonedDateTime(DateTime(2016, 1, 1, 3, 0, 0), TimeZone("America/Winnipeg"))
+horizon_start = Dates.trunc(due_date, Dates.Day) + Dates.Day(1) + Dates.Hour(1)
+horizon_finish = Dates.trunc(a, Dates.Day) + Dates.Day(2)
+target_dates = horizon_start:Dates.Hour(1):horizon_finish
+```
+
+While it's a credit to Julia (and the TimeZones.jl package) that its adjuster functions can
+handle that, in practice it's a lot harder to interpret (and code correctly). This package
+aims to simplify this process by providing easy-to-use horizon generator functions (tasks)
+take a due date as input and produce target dates. For example:
+
+```julia
+using TimeZones
+due_date = ZonedDateTime(DateTime(2016, 1, 1, 3, 0, 0), TimeZone("America/Winnipeg"))
+target_dates = horizon_next_day(due_date)
+```
+
+The horizon functions (like `horizon_next_day`) will have optional parameters (like how many
+days ahead you want the target dates to begin) with reasonable defaults (in this case,
+"start with the next day").
+
+## More Than Just Horizons!
+
+Horizon `Task`s are designed to address these issues, replacing the following components of
+the Matlab Autopredictor:
+
+  * Forecast **horizons**. For a given due (batch) date, the Matlab Autopredictor would make
+    predictions for a series of horizons, each of which were `timedelta`s (`Period`s). These
+	horizons would eventually be added to the due (batch) dates to obtain the target date.
+	Instead of returning `Period`s, `Horizons.jl` returns `DateTime`s (or `ZonedDateTime`s)
+	directly.
+  * Static and dynamic **offsets**. When defining the data source features to use as model
+    input, the Matlab Autopredictor took a base date (either the due date or the target date
+	of the forecast) and for both static (e.g., three hours) and dynamic (e.g., same hour of
+	the week) offsets.
+
+**TODO:** Ensure that we can have offets based on target date (often used for forecast
+data, such as NWP) and offsets based on due date (often used for "most recent actuals" data,
+such as recent actual generation). It's important to make the distinction between the two
+non-ambiguous, as confusing one for the other will result in cheating.
 
 ## Examples
 
-**TODO:** Provide examples of horizons, static offsets, dynamic offsets, etc.
+**TODO:** Provide examples of horizons, static offsets, dynamic offsets, etc. beyond those
+in the Overview section.
--- src/Horizons.jl
+++ src/Horizons.jl
@@ -1,5 +1,209 @@
 module Horizons
 
-# package code goes here
+using Base.Dates
+
+
+# We'll want basic horizon constructors for:
+#   X:(Z):Y hours
+#   every [hour, minute, ...] in a [day, week, ...]
+
+
+#=
+Generator functions in Julia:
+
+    function producer()
+        produce("start")
+        for n=1:4
+            produce(2n)
+        end
+        produce("stop")
+    end
+
+    t(args) = @task producer(args)
+
+To consume:
+
+    println(consume(t))
+
+Or:
+
+    for i in t
+        println(t)
+    end
+
+A `Task` constructor requires a zero-argument function. In order for a producer to be
+parameterized, you'll need to create an anonymous function or use the `@task` macro:
+
+    task = Task(() -> producer(args))
+
+Or:
+
+    task = @task producer(args)
+=#
+
+
+#=
+NOTE: The tasks/generators for horizons could be replaced by simple `StepRange`s, now that
+that Gem got those working for `ZonedDateTime`s, but `StepRange`s are to simplistic to work
+for the dynamic data offsets (offsets that can be defined like, "most recent data available
+for the same hour of week as the target"). The plan is currently to use taks for everything,
+but this is still kind of up in the air right now. (Another possibility would be to separate
+horizons and static/dynamic data offsets again, but as Gem and Curt discussed it seems like
+a good idea to couple these two concepts.)
+=#
+
+
+# HORIZON FUNCTIONS
+# These are the topmost things that people will use.
+# A horizon function takes a `TimeType` and a set of other criteria and returns a `Task`.
+# Whenever `consume` is called on the `Task` (typically via a foreach loop) a `DateTime` is
+# returned, representing a target date (not an offset).
+
+"""
+Basic horizons.
+"""
+function horizon(
+    d::TimeType, range::Range;
+    include::Function=x->true, exclude::Function=x->false
+)
+    @task horizon_producer(d + range, include, exclude)
+end
+
+function horizon(
+    d::TimeType, range::Range, period::DataType=Hour;
+    include::Function=x->true, exclude::Function=x->false
+)
+    @task horizon_producer(d + map(period, range), include, exclude)
+end
+# I'm not convinced that we want the above version. —Gem
+
+"""
+Horizons for each hour (or whatever) of a day (or whatever). (Uh, clean that up and name
+properly.) See the example in the README.
+"""
+function horizon_next_day(
+    due_date::TimeType, resolution::Period=Hour(1),
+    days_ahead::Period=Day(1), days_covered::Period=Day(1);
+    include::Function=x->true, exclude::Function=x->false
+)
+    start = trunc(due_date, Day) + days_ahead + resolution
+    finish = trunc(a, Day) + days_ahead + days_covered
+    @task horizon_producer(start:resolution:finish, include, exclude)
+end
+
+# TODO: Test all horizon functions for normal dates, spring forward, fall back (both on due
+# date and target date).
+
+
+
+
+
+# DATA SOURCE FUNCTIONS
+# These are also top-level, but are specialized versions of horizons designed for static
+# and dynamic offsets.
+
+# TODO: Prototype and write data source tasks.
+
+# TODO: Because these will take available dates into account, they will need to know what
+# `now` is. Support passing in a value for `now` (or a function?); defaults to `Base.now()`.
+
+
+
+"""
+For each element in a range, return a target date. The kwargs `include` and `exclude` are
+`DateFunction`s; if supplied, they indicate which elements should be included (default: all)
+and/or excluded (default: none). This may be helpful when creating dynamic offsets for data
+source functions, for example.
+"""
+function horizon_producer(
+    range::Range; include::Function=x->true, exclude::Function=x->false
+)
+    # Combine include and exclude functions into a single include function.
+    master_include = x -> include(x) && ~exclude(x)
+
+    for d in recur(master_include, range)
+        produce(d)
+    end
+end
+
+# TODO: Test. This.
+
+
+
+
+# DATE FUNCTIONS
+# These will be used by the data source functions to e.g. match hour of day or hour of week.
+
+#=
+A `DateFunction` is an "inclusion" funtion used by adjusters that takes a single `TimeType`
+and returns true when it matches certain criteria. When we're trying to match hour of day or
+hour of week the criterion we're trying to match (the HOD or HOW of another date) is itself
+variable, which complicates things. For this reason these functions are `DateFunction`
+factories: they take a single target `TimeType` (the date we're trying to match) and return
+a lambda function which is the `DateFunction` we'll use for inclusion.
+=#
+
+"`DateFunction` factory. Takes a target hour of day and returns a `DateFunction` to match."
+match_hourofday(target::TimeType) = d -> hour(d) == hour(target)
+match_hourofday(target::Integer) = d -> hour(d) == target
+
+"`DateFunction` factory. Takes a target hour of week and returns a `DateFunction` to match."
+match_hourofweek(target::TimeType) = d -> hourofweek(d) == hourofweek(target)
+match_hourofweek(target::Integer) = d -> hourofweek(d) == target
+
+
+
+
+
+# UTILITY FUNCTIONS
+# Generic utility functions required by Horizons.jl (but not necessarily specific to it).
+
+"""
+Returns an integer indicating the hour of week (0 through 167). For locales in which weeks
+can have more or fewer than 168 hours (those that observe DST), two consecutive hours may
+return the same result (or an integer may be skipped, as the case may be). This was judged
+preferable to having a potential 169th hour for our use case.
+"""
+hourofweek(d::TimeType) = (dayofweek(d) - 1) * 24 + hour(d)
+
+
+# Allows arithmetic between a single `DateTime`/`ZonedDateTime` and a range of `Period`s.
+.+{T<:Period}(x::TimeType, r::Range{T}) = (x + first(r)):step(r):(x + last(r))
+.+{T<:Period}(r::Range{T}, x::TimeType) = x .+ r
++{T<:Period}(r::Range{T}, x::TimeType) = x .+ r
++{T<:Period}(x::TimeType, r::Range{T}) = x .+ r
+.-{T<:Period}(r::Range{T}, x::TimeType) = (first(r) - x):step(r):(last(r) - x)
+-{T<:Period}(r::Range{T}, x::TimeType) = r .- x
+
+
+# Math between a single `Period` and a range of `DateTime`/`ZonedDateTime`s already works.
+# Stolen from usr/local/Cellar/julia/0.4.1/share/julia/base/dates/ranges.jl:
+
+#=
+.+{T<:TimeType}(x::Period, r::Range{T}) = (x+first(r)):step(r):(x+last(r))
+.+{T<:TimeType}(r::Range{T},x::Period) = x .+ r
++{T<:TimeType}(r::Range{T},x::Period) = x .+ r
++{T<:TimeType}(x::Period,r::Range{T}) = x .+ r
+.-{T<:TimeType}(r::Range{T},x::Period) = (first(r)-x):step(r):(last(r)-x)
+-{T<:TimeType}(r::Range{T},x::Period) = r .- x
+=#
+
+
+
+
+
+
+#=
+Previous version of what is now `horizon_next_day`. We're doing this differently. (But I'm
+keeping this code here in case it's useful later, or we change our minds again.)
+
+"All horizons for a single day. Note that the input is of type `Date`, not `DateTime`."
+function single_day(
+    d::Date, step::Period=Hour(1);
+    include::Function=x->true, exclude::Function=x->false
+)
+    @task horizon_producer(DateTime(d):step:DateTime(d + Day(1)) - Second(1), include, exclude)
+end
+=#
 
 end
